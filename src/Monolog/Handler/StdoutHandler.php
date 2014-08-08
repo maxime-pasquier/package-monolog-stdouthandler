64b5c71eafae0095712c84c727648c7657501904
Maxime <maxoo.design@free.fr>
2014-08-08 10:19:41 +0200

    [cosmetic] checkstyle

--- src/Monolog/Handler/StdoutHandler.php
+++ src/Monolog/Handler/StdoutHandler.php
@@ -10,8 +10,7 @@ use Monolog\Formatter\ColorLineFormatter;
  */
 class StdoutHandler extends StreamHandler
 {
-    const
-        FORMAT = "%message%\n";
+    const FORMAT = "%message%\n";
     /**
      * @param integer $level  The minimum logging level at which this handler will be triggered
      * @param Boolean $bubble Whether the messages that are handled can bubble up the stack or not
